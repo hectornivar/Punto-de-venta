15e1e95bf8eb19dd85203c3d15300f1a9375ab03
Hector Rafael Nivar Gomez <hectornivar123@gmail.com>
2026-04-15 11:40:23 +0800

    Interfaz de la ventana de ventas preparado para agregar funciones

--- ventas.py
+++ ventas.py
@@ -3,8 +3,99 @@
 
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk, messagebox, simpledialog
 from PIL import Image, ImageTk
 
 
 class Ventas(tk.Frame):
-    pass
+
+    def __init__(self, padre):
+        super().__init__(padre)
+        self.widgets()
+
+    def widgets(self):
+        labelframe = tk.LabelFrame(self, font="Arial 12 bold", bg="lightblue")
+        labelframe.place(x=25, y=30, width=1045, height=180)
+
+        label_cliente = tk.Label(
+            labelframe, text="Cliente:", font="Arial 14 bold", bg="lightblue")
+        label_cliente.place(x=10, y=11)
+        self.entry_cliente = ttk.Combobox(labelframe, font="Arial 14 bold")
+        self.entry_cliente.place(x=120, y=8, width=200, height=40)
+
+        label_producto = tk.Label(
+            labelframe, text="Producto:", font="Arial 14 bold", bg="lightblue")
+        label_producto.place(x=10, y=70)
+        self.entry_producto = ttk.Combobox(labelframe, font="Arial 14 bold")
+        self.entry_producto.place(x=120, y=66, width=200, height=40)
+
+        label_cantidad = tk.Label(
+            labelframe, text="Cantidad:", font="Arial 14 bold", bg="lightblue")
+        label_cantidad.place(x=500, y=11)
+        self.entry_cantidad = ttk.Entry(labelframe, font="Arial 14 bold")
+        self.entry_cantidad.place(x=610, y=8, width=100, height=40)
+
+        self.label_stock = tk.Label(
+            labelframe, text="Stock: ", font="Arial 14 bold", bg="lightblue")
+        self.label_stock.place(x=500, y=70)
+
+        self.factura = tk.Label(
+            labelframe, text="Factura", font="Arial 14 bold", bg="lightblue")
+        self.factura.place(x=750, y=11)
+
+        boton_agregar = tk.Button(
+            labelframe, text="Agregar", font="Arial 14 bold")
+        boton_agregar.place(x=90, y=120, width=200, height=40)
+
+        boton_eliminar = tk.Button(
+            labelframe, text="Eliminar", font="Arial 14 bold")
+        boton_eliminar.place(x=310, y=120, width=200, height=40)
+
+        boton_editar = tk.Button(
+            labelframe, text="Editar", font="Arial 14 bold")
+        boton_editar.place(x=530, y=120, width=200, height=40)
+
+        boton_limpiar = tk.Button(
+            labelframe, text="Limpiar", font="Arial 14 bold")
+        boton_limpiar.place(x=750, y=120, width=200, height=40)
+
+        treFrame = tk.Frame(self, bg='white')
+        treFrame.place(x=70, y=220, width=980, height=300)
+
+        scrol_y = ttk.Scrollbar(treFrame)
+        scrol_y.pack(side=RIGHT, fill=Y)
+
+        scrol_x = ttk.Scrollbar(treFrame, orient=HORIZONTAL)
+        scrol_x.pack(side=BOTTOM, fill=X)
+
+        self.tre = ttk.Treeview(treFrame, yscrollcommand=scrol_y.set, xscrollcommand=scrol_x.set, height=40, columns=(
+            'Factura', 'Cliente', 'Producto', 'Precio', 'Cantidad', 'Total'), show='headings')
+        self.tre.pack(expand=True, fill=BOTH)
+
+        scrol_y.config(command=self.tre.yview)
+        scrol_x.config(command=self.tre.xview)
+
+        self.tre.heading('Factura', text='Factura')
+        self.tre.heading('Cliente', text='Cliente')
+        self.tre.heading('Producto', text='Producto')
+        self.tre.heading('Precio', text='Precio')
+        self.tre.heading('Cantidad', text='Cantidad')
+        self.tre.heading('Total', text='Total')
+
+        self.tre.column('Factura', width=70, anchor='center')
+        self.tre.column('Cliente', width=250, anchor='center')
+        self.tre.column('Producto', width=250, anchor='center')
+        self.tre.column('Precio', width=120, anchor='center')
+        self.tre.column('Cantidad', width=120, anchor='center')
+        self.tre.column('Total', width=150, anchor='center')
+
+        self.label_precio_total = tk.Label(
+            self, text='Precio a pagar: $0', bg='lightblue', font='Arial 18 bold')
+        self.label_precio_total.place(x=680, y=550)
+
+        boton_pagar = tk.Button(self, text='Pagar', font='Arial 14 bold')
+        boton_pagar.place(x=70, y=550, width=180, height=40)
+
+        boton_ver_ventas = tk.Button(
+            self, text='Ver Ventas', font='Arial 14 bold')
+        boton_ver_ventas.place(x=290, y=550, width=280, height=40)
